7512f0886ee551eba6853320d5ad7c656d732ff0
Tarik Firkatoune <148897375+TarikFirkatoune@users.noreply.github.com>
2025-03-09 21:39:17 -0400

    front end work

---
+++ constants/theme.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+    primary: "#4ADE80",
+    secondary: "#2DD48F",
+    surface: "#1A1A1A",
+    surfaceLight: "#2A2A2A",
+    white: "#FFFFFF",
+    grey: "#9CA3AF",
+    blue: "#007AFF",
+    red: "#DC143C",
+    
+} as const;
